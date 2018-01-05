3b19de144224017013426afb100fb1f8fd49b2a3
thomas <thomas.cocchiara@gmail.com>
2018-01-05 13:58:32 +0100

    Now the Result class implements the ArrayAccess interface

--- src/Result.php
+++ src/Result.php
@@ -4,7 +4,7 @@ namespace MongoDriver;
 
 use PHPAnnotations\Reflection\TC_Reflector;
 
-class Result implements \IteratorAggregate
+class Result implements \IteratorAggregate, \ArrayAccess
 {
     private $items = [];
     private $db = '';
@@ -53,14 +53,10 @@ class Result implements \IteratorAggregate
     }
 
     /**
-     * Retrieve an external iterator
-     * @link http://php.net/manual/en/iteratoraggregate.getiterator.php
-     * @return \Traversable An instance of an object implementing <b>Iterator</b> or
-     * <b>Traversable</b>
-     * @since 5.0.0
+     * Populates the field with documents matching the value from the referenced collection.
+     * @param string $field
+     * @return $this
      */
-    public function getIterator() { return new \ArrayIterator($this->items); }
-
     public function populate($field)
     {
         if (!Configs::isModelRegistered($this->db, $this->collection)) return $this;
@@ -92,4 +88,67 @@ class Result implements \IteratorAggregate
 
         return $this;
     }
+
+    /**
+     * Retrieve an external iterator
+     * @link http://php.net/manual/en/iteratoraggregate.getiterator.php
+     * @return \Traversable An instance of an object implementing <b>Iterator</b> or
+     * <b>Traversable</b>
+     * @since 5.0.0
+     */
+    public function getIterator() { return new \ArrayIterator($this->items); }
+
+    /**
+     * Whether a offset exists
+     * @link http://php.net/manual/en/arrayaccess.offsetexists.php
+     * @param mixed $offset <p>
+     * An offset to check for.
+     * </p>
+     * @return boolean true on success or false on failure.
+     * </p>
+     * <p>
+     * The return value will be casted to boolean if non-boolean was returned.
+     * @since 5.0.0
+     */
+    public function offsetExists($offset) { return isset($this->items[$offset]); }
+
+    /**
+     * Offset to retrieve
+     * @link http://php.net/manual/en/arrayaccess.offsetget.php
+     * @param mixed $offset <p>
+     * The offset to retrieve.
+     * </p>
+     * @return mixed Can return all value types.
+     * @since 5.0.0
+     */
+    public function offsetGet($offset) { return isset($this->items[$offset]) ? $this->items[$offset] : null; }
+
+    /**
+     * Offset to set
+     * @link http://php.net/manual/en/arrayaccess.offsetset.php
+     * @param mixed $offset <p>
+     * The offset to assign the value to.
+     * </p>
+     * @param mixed $value <p>
+     * The value to set.
+     * </p>
+     * @return void
+     * @since 5.0.0
+     */
+    public function offsetSet($offset, $value)
+    {
+        if (is_null($offset)) $this->items[] = $value;
+        else $this->items[$offset] = $value;
+    }
+
+    /**
+     * Offset to unset
+     * @link http://php.net/manual/en/arrayaccess.offsetunset.php
+     * @param mixed $offset <p>
+     * The offset to unset.
+     * </p>
+     * @return void
+     * @since 5.0.0
+     */
+    public function offsetUnset($offset) { unset($this->items[$offset]); }
 }
